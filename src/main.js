f2b3f150821c6939c8bbc63fd8ba7ebf97c6d079
LnRabbiT <Decha_L@cmu.ac.th>
2022-05-05 17:40:29 +0700

    feat: Add axios services

--- src/main.js
+++ src/main.js
@@ -4,4 +4,5 @@ import router from "./router";
 import store from "./store";
 import "./index.css";
 import "@fontsource/prompt";
+import "@/services/AxiosInterceptorSetup.js";
 createApp(App).use(store).use(router).mount("#app");
