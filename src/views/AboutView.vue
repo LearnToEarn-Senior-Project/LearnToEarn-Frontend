241c28d6c48232fbeb232cc735dffbd7d8728504
LnRabbiT <Decha_L@cmu.ac.th>
2022-06-11 14:57:26 +0700

    fix: delete the error component

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,31 +5,15 @@
     <h1>This is an about page</h1>
     <SubmitButton :click="goToAddRewardPage" class="mr-4" text="Add Reward" />
     <SubmitButton :click="getClassroom" text="Test Get Classroom" />
-    <div class="mt-4">
-      <BaseInput name="name" placeholder="Text Box" type="text" />
-      <BaseInput name="textarea" placeholder="Text Area" type="textarea" />
-    </div>
-    <ClassroomCard />
-    <!-- <div class="flex gap-4 mb-4"><RewardCard /> </div> -->
-    <StudentCard class="mb-4" />
     <AssignmentCard />
   </div>
 </template>
 <script>
-import ClassroomServices from "@/services/ClassroomServices.js";
-import SubmitButton from "@/components/button/children/SubmitButton";
-import BaseInput from "@/components/input/BaseInput";
-import RewardCard from "@/components/card/RewardCard";
-import ClassroomCard from "@/components/card/ClassroomCard";
 import StudentCard from "@/components/card/StudentCard";
 import AssignmentCard from "@/components/card/AssignmentCard";
 import { showAlert } from "@/hooks/sweet-alert/sweet-alert";
 export default {
   components: {
-    SubmitButton,
-    BaseInput,
-    RewardCard,
-    ClassroomCard,
     StudentCard,
     AssignmentCard,
   },
